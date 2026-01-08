34a772b9494cf7f51e9e1f9046fe02d4a4357ece
Amarosen00 <amaroxiller@gmail.com>
2026-01-08 19:24:11 -0300

    Fix: Corregido error de exportacion en Dubtes.tsx ,Se ha agregado un codigo placeholder

---
+++ src/pages/Dubtes.tsx
@@ -0,0 +1,12 @@
+// src/pages/Dubtes.tsx
+
+const Dubtes = () => {
+    return (
+        <div>
+            <h1>Sección de Dudas</h1>
+            <p>Esta página está en construcción.</p>
+        </div>
+    );
+};
+
+export default Dubtes;
